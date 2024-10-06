a511c64bd3775e1d03d9291e93574269c90c1209
Mohammed Ali Ayad <maaone997@gmail.com>
2024-10-06 12:32:17 +0300

    Add files via upload

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+
+    using System;
+
+    // single inheritance
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle myvehicle = new Vehicle();
+            myvehicle.DisplayInfo();
+
+            Car mycar = new Car();
+            mycar.DisplayInfo();
+
+            Truck myTruck = new Truck();
+            myTruck.DisplayInfo();
+
+        }
+        //Create a base class called Vehicle with properites
+        public class Vehicle
+        {
+          //properties like Make, Model, and Year
+
+            public string Make = "japan";
+            public string Model = "Nisan";
+            public string Year = " 1992";
+
+            // Adding a method DisplayInfo() in Vehicle to display the details of the vehicle
+            public virtual string DisplayInfo()
+            {
+                string msg = Make;
+                Console.WriteLine(msg);
+                return msg;
+            }
+        }
+        //create two derived classes, Car and Truck
+        public class    Car : Vehicle
+        {
+            public new void DisplayInfo()
+            {
+                Console.WriteLine(Make + Model + Year);
+            }
+        }
+        public class Truck : Vehicle
+        {
+            public new void DisplayInfo()
+            {
+
+                Console.WriteLine(Make + Model + Year);
+            }
+        }
+    }
+}
+    
---
+++ ConsoleApp1/Class2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    // Abstract class
+    abstract class Animal
+    {
+        // Abstract method (does not have a body)
+        public abstract void makeSound();
+        // Regular method
+        public void Sleep()
+        {
+            Console.WriteLine("make sound");
+        }
+    }
+
+    // Derived class (inherit from Animal)
+    class Dog : Animal
+    {
+        public override void makeSound()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("barking");
+        }
+
+
+        class Cat : Animal
+        {
+            public override void makeSound()
+            {
+                // The body of animalSound() is provided here
+                Console.WriteLine("meo");
+            }
+
+            static void Main(string[] args)
+            {
+                Dog myDog = new Dog(); // Create a dog object
+                myDog.makeSound();  // Call the abstract method
+
+                Cat myCat = new Cat(); // Create a cat object
+                myCat.makeSound();  // Call the abstract method
+
+
+            }
+        }
+    }
+}
